ca303199578f7dec4fd17a4ecd22d374f01967e9
Kerry Buckley <kerryjbuckley@gmail.com>
2015-10-03 13:55:35 +0100

    Create fake user repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,18 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: 1, name: "Buffy Summers", username: "buffy", password: "slayer"},
+      %Rumbl.User{id: 2, name: "Willow Rosenberg", username: "willow", password: "witch"},
+      %Rumbl.User{id: 1, name: "Xander Harris", username: "xander", password: "sidekick"},
+    ]
+  end
+
+  def get(module, id) do
+    all(module) |> Enum.find(fn map -> map.id == id end)
+  end
+
+  def get_by(module, params) do
+    all(module) |> Enum.find(fn map ->
+      Enum.all?(params, fn {key, value} -> Map.get(map, key) == value end) end)
+  end
 end
