653657fa24f3d23f681196c3e07b4edc05eb7b14
Kerry Buckley <kerryjbuckley@gmail.com>
2015-10-04 11:49:09 +0100

    Move users into the database

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,19 +1,3 @@
 defmodule Rumbl.Repo do
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: 1, name: "Buffy Summers", username: "buffy", password: "slayer"},
-      %Rumbl.User{id: 2, name: "Willow Rosenberg", username: "willow", password: "witch"},
-      %Rumbl.User{id: 1, name: "Xander Harris", username: "xander", password: "sidekick"},
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    all(module) |> Enum.find(fn map -> map.id == String.to_integer(id) end)
-  end
-
-  def get_by(module, params) do
-    all(module) |> Enum.find(fn map ->
-      Enum.all?(params, fn {key, value} -> Map.get(map, key) == value end) end)
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,12 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :username, :string
+    field :name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
 end
